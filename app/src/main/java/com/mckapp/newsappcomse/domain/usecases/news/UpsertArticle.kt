b14822d5b2f1b384114cbbc9c55710dd7b6fff75
Mustafa Cantürk Karabulut <karabulut_3828@hotmail.com>
2024-04-14 01:56:45 +0300

    Refactor Code
    -Hata durumları için görünüm durumlarını ayarladım
    -Home screendeki görünüm bozukluluğunu düzelttim.

--- app/src/main/java/com/mckapp/newsappcomse/domain/usecases/news/UpsertArticle.kt
+++ app/src/main/java/com/mckapp/newsappcomse/domain/usecases/news/UpsertArticle.kt
@@ -1,6 +1,5 @@
 package com.mckapp.newsappcomse.domain.usecases.news
 
-import com.mckapp.newsappcomse.data.local.NewsDao
 import com.mckapp.newsappcomse.domain.model.Article
 import com.mckapp.newsappcomse.domain.repository.NewsRepository
 
